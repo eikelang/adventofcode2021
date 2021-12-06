f3418b0ed7f6478581c705de73f0f2cccf888f30
Eike Lang <eike.lang@valtech.com>
2021-12-06 09:09:48 +0100

    Once you do it right...

--- src/test/java/Day6.java
+++ src/test/java/Day6.java
@@ -1,9 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.MatchResult;
 import java.util.stream.Collectors;
 
@@ -13,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Day6 {
 
-    private List<AtomicInteger> readFile() throws IOException {
+    private Map<Integer, Long> readFile() throws IOException {
         var inputStream = Day6.class.getResourceAsStream("/day6.txt");
         var fis = new InputStreamReader(inputStream);
         var buf = new BufferedReader(fis);
@@ -21,41 +20,37 @@ class Day6 {
         scanner.useDelimiter(",");
         return scanner.findAll("\\d")
                 .map(MatchResult::group)
-                .map(s -> new AtomicInteger(Integer.parseInt(s)))
-                .collect(Collectors.toList());
+                .map(Integer::parseInt)
+                .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
     }
 
     @Test
     void growLanternfishForEightyDays() throws IOException {
-        final int fishCount = growLanternFishForDays(80);
+        final long fishCount = growLanternFishForDays(80);
         assertThat(fishCount).isEqualTo(390923);
     }
 
     @Test
     void growLanternfishFor256Days() throws IOException {
-        final int fishCount = growLanternFishForDays(128);
-        System.out.println(Integer.MAX_VALUE);
-        assertThat(fishCount).isEqualTo(390923);
+        final long fishCount = growLanternFishForDays(256);
+        assertThat(fishCount).isEqualTo(1749945484935L);
     }
 
-    private int growLanternFishForDays(final int days) throws IOException {
-        final List<AtomicInteger> fish = readFile();
+    private long growLanternFishForDays(final int days) throws IOException {
+        final Map<Integer, Long> fish = readFile();
+        for (int i = 0; i <= 8; i++) {
+            fish.putIfAbsent(i, 0L);
+        }
 
         for (int i = 0; i < days; i++) {
-            var newFish = 0;
-            for (final AtomicInteger daysUntilProcreation : fish) {
-                if (daysUntilProcreation.intValue() == 0) {
-                    daysUntilProcreation.set(6);
-                    newFish++;
-                } else {
-                    daysUntilProcreation.decrementAndGet();
-                }
+            final var newFish = fish.getOrDefault(0, 0L);
+            for (int j = 0; j < 8; j++) {
+                fish.put(j, fish.get(j + 1));
             }
-            for (int j = 0; j < newFish; j++) {
-                fish.add(new AtomicInteger(8));
-            }
+            fish.put(6, fish.get(6) + newFish);
+            fish.put(8, newFish);
         }
-        return fish.size();
+        return fish.values().stream().mapToLong(l -> l).sum();
     }
 
 }
