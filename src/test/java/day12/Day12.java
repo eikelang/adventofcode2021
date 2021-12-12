e2ec3cf2544d4ed94ce15882866ff5bd42ff08ec
Eike Lang <eike.lang@valtech.com>
2021-12-12 20:45:58 +0100

    Day 12 2/2 although not nice

--- src/test/java/day12/Day12.java
+++ src/test/java/day12/Day12.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -67,6 +68,21 @@ class Day12 {
     }
 
     @Test
+    void pathsToMapWithRevisit() {
+        final Map<String, Set<String>> passagesByStart = readExample(EXAMPLE)
+                .map(s -> s.split("-"))
+                .flatMap(a -> Stream.of(new CavePassage(a[0], a[1]), new CavePassage(a[1], a[0])))
+                .collect(Collectors.groupingBy(cp -> cp.from, Collectors.mapping(cp -> cp.to, Collectors.toSet())));
+        passagesByStart.values().forEach(v -> v.remove("start"));
+        final List<List<String>> result2 =
+                continuePath(singletonList("start"), passagesByStart, true)
+                        .filter(list -> "end".equals(list.get(list.size() - 1)))
+                        .collect(Collectors.toList());
+        System.out.println(result2);
+        assertThat(result2).hasSize(36);
+    }
+
+    @Test
     void pathsToLargerMap() {
         final Map<String, Set<String>> passagesByStart = readExample(LARGE_SAMPLE)
                 .map(s -> s.split("-"))
@@ -81,6 +97,20 @@ class Day12 {
         assertThat(result2).hasSize(19);
     }
 
+    @Test
+    void pathsToLargerMapWithRevisit() {
+        final Map<String, Set<String>> passagesByStart = readExample(LARGE_SAMPLE)
+                .map(s -> s.split("-"))
+                .flatMap(a -> Stream.of(new CavePassage(a[0], a[1]), new CavePassage(a[1], a[0])))
+                .collect(Collectors.groupingBy(cp -> cp.from, Collectors.mapping(cp -> cp.to, Collectors.toSet())));
+        passagesByStart.values().forEach(v -> v.remove("start"));
+        final List<List<String>> result2 =
+                continuePath(singletonList("start"), passagesByStart, true)
+                        .filter(list -> "end".equals(list.get(list.size() - 1)))
+                        .collect(Collectors.toList());
+        System.out.println(result2);
+        assertThat(result2).hasSize(103);
+    }
 
     @Test
     void pathsToPuzzleMap() {
@@ -97,30 +127,58 @@ class Day12 {
         assertThat(result2).hasSize(4413);
     }
 
+    @Test
+    void pathsToPuzzleMapWithRevisit() {
+        final Map<String, Set<String>> passagesByStart = readFile()
+                .map(s -> s.split("-"))
+                .flatMap(a -> Stream.of(new CavePassage(a[0], a[1]), new CavePassage(a[1], a[0])))
+                .collect(Collectors.groupingBy(cp -> cp.from, Collectors.mapping(cp -> cp.to, Collectors.toSet())));
+        passagesByStart.values().forEach(v -> v.remove("start"));
+        final List<List<String>> result2 =
+                continuePath(singletonList("start"), passagesByStart, true)
+                        .filter(list -> "end".equals(list.get(list.size() - 1)))
+                        .collect(Collectors.toList());
+        System.out.println(result2);
+        assertThat(result2).hasSize(118803);
+    }
+
     private Set<String> neighbours(final String currentCave, final Map<String, Set<String>> availablePaths) {
         return availablePaths.get(currentCave);
     }
 
     private Stream<List<String>> continuePath(final List<String> currentPath,
-            final Map<String, Set<String>> availablePaths, final boolean mayRevisitASmallCave) {
-        System.out.println("Already traversed " + currentPath);
+            final Map<String, Set<String>> availablePaths, final boolean revisitingAllowed) {
+//        System.out.println("Already traversed " + currentPath);
+        final boolean smallCaveAlreadyVisitedTwice =
+                currentPath.stream().filter(cave -> cave.equals(cave.toLowerCase()))
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                        .values()
+                        .stream()
+                        .anyMatch(v -> v > 1);
+        final Set<String> visitedSmallCaves = currentPath.stream().filter(cave -> cave.equals(cave.toLowerCase()))
+                .collect(Collectors.toSet());
         final var endOfCurrentPath = currentPath.get(currentPath.size() - 1);
         final Set<String> pathCandidates = availablePaths.getOrDefault(endOfCurrentPath, emptySet());
-        System.out.println("Can move to " + pathCandidates);
+        if (!revisitingAllowed || smallCaveAlreadyVisitedTwice) {
+            pathCandidates.removeAll(visitedSmallCaves);
+        }
         if (pathCandidates.isEmpty() || "end".equals(endOfCurrentPath)) {
             return Stream.of(currentPath);
         } else {
             return pathCandidates.stream().flatMap(candidate -> continuePath(augmentPath(currentPath, candidate),
-                    reducePaths(candidate, availablePaths, false), false));
+                    reducePaths(candidate, availablePaths, visitedSmallCaves,
+                            revisitingAllowed && !smallCaveAlreadyVisitedTwice),
+                    revisitingAllowed && !smallCaveAlreadyVisitedTwice));
         }
 
     }
 
     private Map<String, Set<String>> reducePaths(final String candidate,
-            final Map<String, Set<String>> availablePaths, final boolean mayRevisitSmallerCave) {
+            final Map<String, Set<String>> availablePaths, final Set<String> smallCavesAlreadyVisited,
+            final boolean revisitingAllowed) {
         final HashMap<String, Set<String>> modifiedAvailablePaths = new HashMap<>();
         availablePaths.forEach((k, v) -> modifiedAvailablePaths.put(k, new HashSet<>(v)));
-        if (candidate.toLowerCase().equals(candidate) && !mayRevisitSmallerCave) {
+        if (candidate.toLowerCase().equals(candidate) && !revisitingAllowed) {
             modifiedAvailablePaths.values().forEach(set -> set.remove(candidate));
         }
         return modifiedAvailablePaths;
